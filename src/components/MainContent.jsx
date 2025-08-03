1d3fc038e097dd7c542e1a1f847c915dc3221483
Letuananh1207 <tuananh12072002@gmail.com>
2025-08-04 01:06:42 +0700

    FE/ Unit

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,13 +5,32 @@ export default function MainContent() {
   return (
     <div className={styles.dashBoard}>
       <div className={styles.mainBoard}>
-        <section className={styles.lesson}>
+        <section className={styles.unit}>
           <header>
             <p>UNIT 1:</p>
             <h2>家族 ・夫婦・恋人の会話</h2>
           </header>
           <hr />
           <div className={styles.lessonList}>
+            <LessonItem />
+            <hr />
+            <LessonItem />
+            <hr />
+            <LessonItem />
+          </div>
+        </section>
+
+        <section className={styles.unit}>
+          <header>
+            <p>UNIT 1:</p>
+            <h2>家族 ・夫婦・恋人の会話</h2>
+          </header>
+          <hr />
+          <div className={styles.lessonList}>
+            <LessonItem />
+            <hr />
+            <LessonItem />
+            <hr />
             <LessonItem />
           </div>
         </section>
